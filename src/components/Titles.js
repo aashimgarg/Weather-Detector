7155a079dbd8ff0e24a797efb6d8959bb1ef9746
aashimgarg <18bcs001@smvdu.ac.in>
2020-05-10 03:48:45 +0530

    weather app :tada:

---
+++ src/components/Titles.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Titles extends React.Component{
+  render(){
+  return (
+    <div>
+        <h1 className="titleh1">
+           Weather Finder.
+        </h1>
+        <h3 className="titleh3">
+        Please enter correct spellings!!
+        </h3>
+    </div>
+  );
+  }
+}
+
+export default Titles;
