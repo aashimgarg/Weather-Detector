7b4045f8300bd1e64570f914c3453580146b17e4
aashimgarg <18bcs001@smvdu.ac.in>
2020-05-10 04:15:27 +0530

    Updates

---
+++ precache-manifest.ab9e8629ce5ab182a2b4ddff0a882932.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25509584fa9be1e9516528b53f6dc1a0",
+    "url": "/Weather-Detector/index.html"
+  },
+  {
+    "revision": "78116e31889d505ada93",
+    "url": "/Weather-Detector/static/css/2.8dded22f.chunk.css"
+  },
+  {
+    "revision": "fbe39477a4d66eccaade",
+    "url": "/Weather-Detector/static/css/main.39c05e46.chunk.css"
+  },
+  {
+    "revision": "78116e31889d505ada93",
+    "url": "/Weather-Detector/static/js/2.bcc9511c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-Detector/static/js/2.bcc9511c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbe39477a4d66eccaade",
+    "url": "/Weather-Detector/static/js/main.01fd53ee.chunk.js"
+  },
+  {
+    "revision": "e64d10de3285d3ed7225",
+    "url": "/Weather-Detector/static/js/runtime-main.a49bffc6.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Weather-Detector/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Weather-Detector/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Weather-Detector/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Weather-Detector/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Weather-Detector/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "f9a5512a61df2daed795a8711590c209",
+    "url": "/Weather-Detector/static/media/storm.f9a5512a.jpeg"
+  }
+]);
